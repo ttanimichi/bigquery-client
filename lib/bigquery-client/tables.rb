934f1ff2125d7f7e8295d812baec5ccb2421eff8
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-02-09 20:28:36 +0900

    Implements Tables API

--- lib/bigquery-client/tables.rb
+++ lib/bigquery-client/tables.rb
@@ -17,7 +17,44 @@ module BigQuery
       handle_error(result) if result.error?
     end
 
+    def list_tables(options = {})
+      result = access_api(
+        api_method: bigquery.tables.list,
+        parameters: options
+      )
+      handle_error(result) if result.error?
+      JSON.parse(result.body)
+    end
+
+    def update_table(name, options = {})
+      result = access_api(
+        api_method: bigquery.tables.update,
+        parameters: {
+          tableId: name
+        },
+        body_object: options
+      )
+      handle_error(result) if result.error?
+      JSON.parse(result.body)
+    end
+
+    def patch_table(name, options = {})
+      result = access_api(
+        api_method: bigquery.tables.patch,
+        parameters: {
+          tableId: name
+        },
+        body_object: options
+      )
+      handle_error(result) if result.error?
+      JSON.parse(result.body)
+    end
+
     def fetch_schema(table)
+      fetch_table_info(table)['schema']['fields']
+    end
+
+    def fetch_table_info(table)
       result = access_api(
         api_method: bigquery.tables.get,
         parameters: {
@@ -25,7 +62,17 @@ module BigQuery
         }
       )
       handle_error(result) if result.error?
-      JSON.parse(result.body)['schema']['fields']
+      JSON.parse(result.body)
+    end
+
+    def drop_table(name)
+      result = access_api(
+        api_method: bigquery.tables.delete,
+        parameters: {
+          tableId: name
+        }
+      )
+      handle_error(result) if result.error?
     end
   end
 end

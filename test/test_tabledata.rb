bb90ab94ed409f2bc3f4fd134ec44988546876d2
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-03-15 15:43:29 +0900

    --wip--

--- test/test_tabledata.rb
+++ test/test_tabledata.rb
@@ -2,27 +2,28 @@ require 'helper'
 
 class TablesTest < Test::Unit::TestCase
   def test_insert
-    prepare_table(__method__)
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    $client.create_table(table_name, schema)
     result = $client.insert(table_name, bar: "foo")
     assert { result['kind'] == 'bigquery#tableDataInsertAllResponse' }
   end
 
   def test_insert_with_array
-    prepare_table(__method__)
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    $client.create_table(table_name, schema)
+    result = $client.list_tabledata(table_name)
     rows = [{ bar: "foo" },  { bar: "foo2" },  { bar: "foo3" }]
     result = $client.insert(table_name, rows)
     assert { result['kind'] == 'bigquery#tableDataInsertAllResponse' }
   end
 
   def test_list_tabledata
-    prepare_table(__method__)
-    result = $client.list_tabledata(table_name)
-    assert { result['kind'] == "bigquery#tableDataList" }
-  end
-
-  def prepare_table(name)
-    table_name = name.to_s
+    table_name = __method__.to_s
     schema = [{ name: 'bar', type: 'string' }]
     $client.create_table(table_name, schema)
+    result = $client.list_tabledata(table_name)
+    assert { result['kind'] == "bigquery#tableDataList" }
   end
 end

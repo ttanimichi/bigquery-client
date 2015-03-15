f78affda29f9988172b7e8ef60378a47f836392a
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-03-15 19:14:18 +0900

    Update BigQuery::Tables

---
+++ test/test_tables.rb
@@ -0,0 +1,41 @@
+require 'helper'
+
+class TablesTest < Test::Unit::TestCase
+  def test_tables
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    $client.create_table(table_name, schema)
+    assert { $client.tables.include?(table_name) }
+  end
+
+  def test_fetch_schema
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    $client.create_table(table_name, schema)
+    result = $client.fetch_schema(table_name)
+    assert { result == [{"name"=>"bar", "type"=>"STRING"}] }
+  end
+
+  def test_create_table
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    before = $client.tables
+    $client.create_table(table_name, schema)
+    after  = $client.tables
+    actual = after - before
+    expected = [table_name]
+    assert { expected == actual }
+  end
+
+  def test_delete_table
+    table_name = __method__.to_s
+    schema = [{ name: 'bar', type: 'string' }]
+    $client.create_table(table_name, schema)
+    before = $client.tables
+    $client.delete_table(table_name)
+    after  = $client.tables
+    actual = before - after
+    expected = [table_name]
+    assert { actual == expected }
+  end
+end

8fd5866e21a1dd2cff27af140f1170afd3d7f730
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-09-16 21:15:33 +0900

    wip [ci skip]

--- lib/bigquery-client/jobs.rb
+++ lib/bigquery-client/jobs.rb
@@ -10,23 +10,6 @@ module BigQuery
       RunQuery.new(self, query, options).call
     end
 
-      # jobs_query_response = jobs_query(query, options)
-      # fields = jobs_query_response['schema']['fields']
-      # # こいつらはインスタンス変数
-      # names = fields.map {|field| field['name'] } # カラム名
-      # types = fields.map {|field| field['type'] } # カラムの型
-      # # こいつもインスタンス変数
-      # records = extract_records(jobs_query_response)
-      # # jobId がたぶんユニークだから jobId が違ったら結合できないようにする、とか
-      # job_id = jobs_query_response['jobReference']['jobId']
-      # page_token = jobs_query_response['pageToken']
-      # while page_token
-      #   query_results_response = query_results(job_id, { pageToken: page_token }.merge(options))
-      #   records += extract_records(query_results_response)
-      #   page_token = query_results_response['pageToken']
-      # end
-      # convert(records, types).map { |values| [names, values].transpose.to_h }
-
     def jobs_query(query, options = {})
       default = { query: query, timeoutMs: 600_000 }
       access_api(

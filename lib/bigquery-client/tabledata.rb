dd1476094e13b2aeb1215c77adab079b44227de9
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-10-13 21:02:33 +0900

    wip [ci skip]

--- lib/bigquery-client/tabledata.rb
+++ lib/bigquery-client/tabledata.rb
@@ -2,6 +2,17 @@
 
 module BigQuery
   module Tabledata
+
+
+    # insert バグってる
+    #
+    # [5] pry(main)> client.insert('test_timestamp', foo: "2015-10-13 20:05:43 +0900")
+    # => {"kind"=>"bigquery#tableDataInsertAllResponse", "insertErrors"=>[{"index"=>0, "errors"=>[{"reason"=>"invalid", "location"=>"Field:foo", "message"=>"Could not parse '2015-10-13 20:05:43 +0900' as a timestamp. Required format is YYYY-MM-DD HH:MM[:SS[.SSSSSS]]"}]}]}
+    # [6] pry(main)> client.insert('test_timestamp', foo: "2015-10-13 20:05:43")
+    # => {"kind"=>"bigquery#tableDataInsertAllResponse"}
+    #
+    # handle_result みたいなメソッドを足す
+    # service クラスにしたほうがいいかも
     def insert(table, arg)
       rows = arg.is_a?(Array) ? arg : [arg]
       access_api(
